469efdd98bfa60f3e1a2238db6a2cf1a632d2414
ZorroKevin <kevindavidzorro@gmail.com>
2025-03-23 16:06:31 -0500

    feat: update title to 'Studybara' and add fullscreen toggle functionality

--- src/App.js
+++ src/App.js
@@ -7,11 +7,28 @@ function App() {
   const [focusTime, setFocusTime] = useState(25 * 60);
   const [breakTime, setBreakTime] = useState(5 * 60);
   const [showModal, setShowModal] = useState(false); // Estado para abrir/cerrar el modal
+  const [isFullscreen, setIsFullscreen] = useState(false);
+
+
+    // Función para alternar pantalla completa
+    const toggleFullscreen = () => {
+      if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen();
+        setIsFullscreen(true);
+      } else {
+        document.exitFullscreen();
+        setIsFullscreen(false);
+      }
+    };
+
 
   return (
     <>
           {/* Sidebar */}
           <Sidebar />
+        <button onClick={toggleFullscreen} >
+        {isFullscreen ? "🔽" : "⛶"}
+      </button>
       <div>
         <Timer focusTime={focusTime} breakTime={breakTime} />
 
