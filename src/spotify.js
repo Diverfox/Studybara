4ea880af56ae12560e88116a202096ea4b3ac60d
Linda Bohorquez <lindavalentina0206@gmail.com>
2025-03-22 22:11:18 -0500

    spotifyCambio

--- src/spotify.js
+++ src/spotify.js
@@ -1,6 +1,3 @@
-Code
-JS
-JavaScript
 // Authorization token that must have been created previously. See : https://developer.spotify.com/documentation/web-api/concepts/authorization
 const token = 'BQDzO1rwhz3w9sLQiCgfQ0NY9pU73AEwk59lIMSJRnf14NRggnRlz2qIp4ykpS-8Ml_z-gN_I8UtTc0dB2bEAUvAhN5bytlea6pIBHG6vK97m9nlfmtIJlLIXWGkdc8lEze4T2F2_dqiMdfrmGlk0kja3dVH6YzhUPjkWQOmid65qvB7H0AfBjKEdGajqLMv3ERh91ul4Tzz_dM2-FpKxGNevQ2UmJ69KZTkLXerqZePbHSeA_3CGnoVQ1vilvuH0MhILma8XPsK2tS8hr_T8dXMi2dAwXvUu1ZJeUCGJRauYMg7EEOyIzJo';
 async function fetchWebApi(endpoint, method, body) {
