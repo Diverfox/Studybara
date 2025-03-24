0b5e57f5540132f67df9438c5a5b8547ca739991
Linda Bohorquez <lindavalentina0206@gmail.com>
2025-03-24 10:57:46 -0500

    ultimos cambio de zorro

--- src/context/SettingsContext.js
+++ src/context/SettingsContext.js
@@ -1,12 +1,11 @@
 import React, { createContext, useState } from "react";
-import fondoChill from "../assets/images/fondochill.jpg";
 
 export const SettingsContext = createContext();
 
 export const SettingsProvider = ({ children }) => {
   const [focusTime, setFocusTime] = useState(25 * 60); // en segundos
   const [breakTime, setBreakTime] = useState(5 * 60); // en segundos
-  const [background, setBackground] = useState(fondoChill); // Fondo predeterminado
+  const [background, setBackground] = useState("default.jpg"); // Fondo predeterminado
 
 
   return (
